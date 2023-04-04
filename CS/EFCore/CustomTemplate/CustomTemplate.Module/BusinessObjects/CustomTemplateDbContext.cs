1ce7b4a44537fced49a6e6dc00aa2f1ef51effe3
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-04 15:01:05 +0400

    init ef

---
+++ CS/EFCore/CustomTemplate/CustomTemplate.Module/BusinessObjects/CustomTemplateDbContext.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+
+namespace CustomTemplate.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class CustomTemplateContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<CustomTemplateEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new CustomTemplateEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class CustomTemplateDesignTimeDbContextFactory : IDesignTimeDbContextFactory<CustomTemplateEFCoreDbContext> {
+	public CustomTemplateEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<CustomTemplateEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CustomTemplate");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new CustomTemplateEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(CustomTemplateContextInitializer))]
+public class CustomTemplateEFCoreDbContext : DbContext {
+	public CustomTemplateEFCoreDbContext(DbContextOptions<CustomTemplateEFCoreDbContext> options) : base(options) {
+	}
+	//public DbSet<ModuleInfo> ModulesInfo { get; set; }
+	public DbSet<ModelDifference> ModelDifferences { get; set; }
+	public DbSet<ModelDifferenceAspect> ModelDifferenceAspects { get; set; }
+	public DbSet<PermissionPolicyRole> Roles { get; set; }
+	public DbSet<CustomTemplate.Module.BusinessObjects.ApplicationUser> Users { get; set; }
+    public DbSet<CustomTemplate.Module.BusinessObjects.ApplicationUserLoginInfo> UserLoginInfos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+        modelBuilder.Entity<CustomTemplate.Module.BusinessObjects.ApplicationUserLoginInfo>(b => {
+            b.HasIndex(nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.LoginProviderName), nameof(DevExpress.ExpressApp.Security.ISecurityUserLoginInfo.ProviderUserKey)).IsUnique();
+        });
+        modelBuilder.Entity<ModelDifference>()
+            .HasMany(t => t.Aspects)
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
